185b0c64577daf9af22d5d2ca49a87a8c2ca71c0
Huy Ly <huy.ly@lazada.com>
2015-10-07 17:57:37 +0700

    Fix bug environment of report server

--- report_server/report.js
+++ report_server/report.js
@@ -2,18 +2,23 @@ var express = require('express');
 
 
 var _ = require('underscore');
-var	os = require('os');
+var os = require('os');
 var argv = require('yargs').argv;
+var bodyParser = require('body-parser');
 
 var autoShutDownMs = (Number(argv.t) === argv.t && argv.t % 1 === 0) ? 1000 * 60 * argv.t : 1000 * 60 * 15;
 var app = express();
 
-
-app.use(express.json());
-app.use(express.urlencoded());
+//app.use(express.json());
+//app.use(express.urlencoded());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 app.use(express.static('report'));
 app.use('/imagediff', express.static('imagediff'));
-app.use('/screenshots', express.static('screenshots'))
+app.use('/resemble', express.static('resemble'));
+app.use('/screenshots', express.static('screenshots'));
 
 // app.get('/', function (req, res) {
 //   res.send('Hello World!');
@@ -25,30 +30,30 @@ var server = app.listen(3000, function () {
 
   console.log('CSS Regression Testing app listening at: ');
   _.each(getAddresses(), function(add){
-	console.log('+ %s:%s', add ,port);
+  console.log('+ %s:%s', add ,port);
   });
 
   //Stop server after 15 mins
   
   if (autoShutDownMs > 0){
-  	console.log('['+new Date()+'] Server will automating shutdown in ' + Math.round(autoShutDownMs / 60000 * 100) / 100 + ' mins');
-  	setTimeout(function(){
-  		server.close();
-  	}, autoShutDownMs);
+    console.log('['+new Date()+'] Server will automating shutdown in ' + Math.round(autoShutDownMs / 60000 * 100) / 100 + ' mins');
+    setTimeout(function(){
+      server.close();
+    }, autoShutDownMs);
   }
   
 
 });
 
 function getAddresses(){
-	var interfaces = os.networkInterfaces(),
-		addresses = [];
+  var interfaces = os.networkInterfaces(),
+    addresses = [];
 
-	_.each(interfaces,function(net) {
-		_.each(net,function(address) {
-			if (address.family == 'IPv4' && !address.internal) addresses.push(address.address);
-		});
+  _.each(interfaces,function(net) {
+    _.each(net,function(address) {
+      if (address.family == 'IPv4' && !address.internal) addresses.push(address.address);
+    });
   });
 
-	return addresses;
+  return addresses;
 }
